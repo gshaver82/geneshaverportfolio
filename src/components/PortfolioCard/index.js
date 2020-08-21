2f83e865e3d6bd96a08607e43629795554a62781
gshaver <geneshaver@gmail.com>
2020-08-21 18:56:18 -0500

    test

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -4,10 +4,11 @@ import "./style.css";
 // href="%PUBLIC_URL%/favicon.ico" 
 
 function PortfolioCard(props) {
+  // const screenshot = props.screenshot;
+  // console.log(screenshot);
   return (
     <div className="card">
       <div className="content">
-
         <div className="projecttitle">
           <strong>{props.TitleOfProject}</strong>
         </div>
@@ -17,12 +18,14 @@ function PortfolioCard(props) {
 
       </div>
       <div className="img-container">
-        <a href={props.screenshot}>
+        {/* <a href={props.screenshot}>
           <img src={props.screenshot } alt={props.TitleOfProject} />
 
+        </a> */}
+        <a href={props.screenshot}>
+          <img src={props.screenshot} alt={props.TitleOfProject} />
+
         </a>
-        {/* this does not work */}
-        {/* <img alt="screenshot" src={require(props.screenshot)} /> */}
       </div>
     </div>
   );
